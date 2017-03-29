7ede1bfea0a157b94bc274a87385c0402f02ea1a
Uzair-wal <ukhan@westagilelabs.com>
2017-03-29 12:10:46 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 3000
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './dev/js/index.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader'
+            }
+        ]
+    },
+    output: {
+        path: '/home/uzair/react-redux-1/React-Redux-Boilerplate-master/src/js/bundle.min.js',
+        filename: 'js/bundle.min.js'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+};
